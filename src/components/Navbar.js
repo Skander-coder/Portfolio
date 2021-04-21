a9f1f79e2c8ea9a9237b8b8f0289033597e044c4
skandermsalmi <msalmiskander@gmail.com>
2021-04-21 01:26:33 +0100

    Navbar Completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+ import { faBars} from '@fortawesome/free-solid-svg-icons';
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+  <a className="navbar-brand logo" href="https://google.com"><i class="fas fa-user-alt"></i> NEW HORIZON</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <FontAwesomeIcon icon={faBars} style={{color:'#FFF'}}/>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="https://google.com">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://google.com">about me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://google.com">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://google.com">how work</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://google.com">portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://google.com">Contacts</a>
+      </li>
+    </ul>
+    
+  </div>
+  </div>
+</nav>
+    )
+}
+
+export default Navbar
